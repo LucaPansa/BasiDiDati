aa4f251a0ffc7a1a1c4c425af2343a64c6090ad8
l.pansa <l.pansa@studenti.unibg.it>
2018-05-23 13:24:24 +0200

    Aggiunti POST e controller notes e integrazione DB in SQLite

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from time import time
 from random import randint
+import sqlite3
+
+
 
 app = Flask(__name__) #creo un'applicazione flask, __name__ richiama il nome del file in modo da avere app e file con lo stesso nome
+
+@app.route('/init_db')
+def init_db():
+    #Creazione DB
+    connection = sqlite3.connect('bdweb.db')
+    connection.execute('DROP TABLE IF EXISTS notes;')
+    connection.execute('CREATE TABLE notes (data VARCHAR);')
+    connection.commit()
+    connection.close()
+    return "DATABASE CREATED"
 
 
 @app.route('/') #Reindirizzamento alla pagina index tramite questo "decoratore", faccio una sorta di instradamento, di fatto rendo la funzione index un controller
@@ -42,7 +55,7 @@ def template_for():
 
 @app.route('/supermarket')
 def supermarket():
-    price = {'strawberry': 2.0,'lemon' :1.0, 'Ciapo':-43214234224332422.0} #dizionario, associa chiave a valore mediante i ":"
+    price = {'strawberry': 2.0, 'lemon':1.0, 'Ciapo':-43214234224332422.0 } #dizionario, associa chiave a valore mediante i ":"
     return render_template("supermarket.html", prices=price)
 
 @app.route('/hello/<name>')
@@ -52,6 +65,28 @@ def hello(name):
 @app.route('/add/<int:x>/<int:y>')
 def add(x,y):
     return str(x + y)
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    x = int(request.form.get('x', 0) or 0)
+    y = int(request.form.get('y', 0) or 0)
+    return render_template('calculator.html', x=x, y=y)
+
+
+
+@app.route('/notes', methods=['GET', 'POST'])
+def notes():
+    note = request.form.get('note') #stesso nome dell'input tag
+    connection = sqlite3.connect('bdweb.db')
+    cursor = connection.cursor()
+    if note:
+        cursor.execute("INSERT INTO notes VALUES (?)", [note])
+        connection.commit()
+
+    cursor.execute('SELECT data FROM notes')
+    notelist = cursor.fetchall()
+
+    return render_template('notes.html', notelist=notelist)
 
 
 
