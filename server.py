8f6c3ab8b0a32cdb32fb0dc26153e95011f358b6
PANSA LUCA <l.pansa@intranet.unibg.it>
2018-05-23 12:01:05 +0200

    File dopo black out, fatte modifiche a server.py e aggiunti 3 nuovi templates

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from time import time
-
+from random import randint
 
 app = Flask(__name__) #creo un'applicazione flask, __name__ richiama il nome del file in modo da avere app e file con lo stesso nome
 
@@ -29,6 +29,20 @@ def template():
 def template_variable():
     timestamp = time()
     return render_template('template_variable.html', timestamp=timestamp)
+
+@app.route('/template_if')
+def template_if():
+    voto = randint(0,32)
+    return render_template('template_if.html', voto=voto)
+
+@app.route('/template_for')
+def template_for():
+    todoList = ['prova1', 'prova2', 'prova3']
+    return render_template('template_for.html', todoList=todoList, len=len(todoList))
+
+@app.route('/supermarket')
+def supermarket:
+    price = {'strawberry',}
 
 
 app.run(debug=True) #Con debug mode off viene visualizzata la generica eccezione "Internal server error"
